b32f8f30bdac3b5aa8ce87b0323d68d88a4033b3
TheAVIATOR559 <howelljacob05@gmail.com>
2021-04-23 16:22:19 -0600

    Building type models workaround

--- hex-trigger/Assets/Scripts/Prefab_Manager.cs
+++ hex-trigger/Assets/Scripts/Prefab_Manager.cs
@@ -9,15 +9,51 @@ public class Prefab_Manager : Singleton<Prefab_Manager>
     private void Awake()
     {
         /* LOAD PREFABS HERE */
-        prefabs.Add(Enums.Prefabs.HEX_EMPTY, Resources.Load<GameObject>("Prefabs/Hex - DEFAULT"));
-        prefabs.Add(Enums.Prefabs.HEX_GARDEN, Resources.Load<GameObject>("Prefabs/Hex - FOOD PROD"));
-        prefabs.Add(Enums.Prefabs.HEX_HOVEL, Resources.Load<GameObject>("Prefabs/Hex - Hovel"));
-        prefabs.Add(Enums.Prefabs.HEX_WORKSHOP, Resources.Load<GameObject>("Prefabs/Hex - INDUSTRY"));
-        prefabs.Add(Enums.Prefabs.HEX_BARRACKS, Resources.Load<GameObject>("Prefabs/Hex - MILITARY"));
-        prefabs.Add(Enums.Prefabs.HEX_RESEARCH_LAB, Resources.Load<GameObject>("Prefabs/Hex - RESEARCH"));
-        prefabs.Add(Enums.Prefabs.HEX_STOCKPILE, Resources.Load<GameObject>("Prefabs/Hex - ISOLIUM PROD & STORAGE"));
-        prefabs.Add(Enums.Prefabs.HEX_GHOST, Resources.Load<GameObject>("Prefabs/Ghost Hex"));
-        prefabs.Add(Enums.Prefabs.HEX_GOD_SEAT, Resources.Load<GameObject>("Prefabs/Hex - God Seat"));
+        prefabs.Add(Enums.Prefabs.HEX_EMPTY, Resources.Load<GameObject>("Prefabs/Hexes/Hex - DEFAULT"));
+        prefabs.Add(Enums.Prefabs.HEX_GHOST, Resources.Load<GameObject>("Prefabs/Hexes/Ghost Hex"));
+        prefabs.Add(Enums.Prefabs.HEX_GOD_SEAT, Resources.Load<GameObject>("Prefabs/Hexes/Hex - God Seat"));
+
+        prefabs.Add(Enums.Prefabs.HEX_GARDEN, Resources.Load<GameObject>("Prefabs/Hexes/Food/Hex - Garden"));
+        prefabs.Add(Enums.Prefabs.HEX_FARM, Resources.Load<GameObject>("Prefabs/Hexes/Food/Hex - Farm"));
+        prefabs.Add(Enums.Prefabs.HEX_ORCHARD, Resources.Load<GameObject>("Prefabs/Hexes/Food/Hex - Orchard"));
+        prefabs.Add(Enums.Prefabs.HEX_RANCH, Resources.Load<GameObject>("Prefabs/Hexes/Food/Hex - Ranch"));
+        prefabs.Add(Enums.Prefabs.HEX_HYDROPONICS_TOWER, Resources.Load<GameObject>("Prefabs/Hexes/Food/Hex - Hydroponics Tower"));
+
+        prefabs.Add(Enums.Prefabs.HEX_HOVEL, Resources.Load<GameObject>("Prefabs/Hexes/Housing/Hex - Hovel"));
+        prefabs.Add(Enums.Prefabs.HEX_COTTAGE, Resources.Load<GameObject>("Prefabs/Hexes/Housing/Hex - Cottage"));
+        prefabs.Add(Enums.Prefabs.HEX_APARTMENT, Resources.Load<GameObject>("Prefabs/Hexes/Housing/Hex - Apartment"));
+        prefabs.Add(Enums.Prefabs.HEX_CONDOMINIUM, Resources.Load<GameObject>("Prefabs/Hexes/Housing/Hex - Condo"));
+        prefabs.Add(Enums.Prefabs.HEX_VILLA, Resources.Load<GameObject>("Prefabs/Hexes/Housing/Hex - Villa"));
+
+        prefabs.Add(Enums.Prefabs.HEX_WORKSHOP, Resources.Load<GameObject>("Prefabs/Hexes/Industry/Hex - Workshop"));
+        prefabs.Add(Enums.Prefabs.HEX_FORGE, Resources.Load<GameObject>("Prefabs/Hexes/Industry/Hex - Forge"));
+        prefabs.Add(Enums.Prefabs.HEX_MILL, Resources.Load<GameObject>("Prefabs/Hexes/Industry/Hex - Mill"));
+        prefabs.Add(Enums.Prefabs.HEX_FOUNDRY, Resources.Load<GameObject>("Prefabs/Hexes/Industry/Hex - Foundry"));
+        prefabs.Add(Enums.Prefabs.HEX_FACTORY, Resources.Load<GameObject>("Prefabs/Hexes/Industry/Hex - Factory"));
+
+        prefabs.Add(Enums.Prefabs.HEX_BARRACKS, Resources.Load<GameObject>("Prefabs/Hexes/Military/Hex - Barracks"));
+        prefabs.Add(Enums.Prefabs.HEX_DORMITORY, Resources.Load<GameObject>("Prefabs/Hexes/Military/Hex - Dormitory"));
+        prefabs.Add(Enums.Prefabs.HEX_GARRISON, Resources.Load<GameObject>("Prefabs/Hexes/Military/Hex - Garrison"));
+        prefabs.Add(Enums.Prefabs.HEX_QUARTERS, Resources.Load<GameObject>("Prefabs/Hexes/Military/Hex - Quarters"));
+        prefabs.Add(Enums.Prefabs.HEX_HEADQUARTERS, Resources.Load<GameObject>("Prefabs/Hexes/Military/Hex - Headquarters"));
+
+        prefabs.Add(Enums.Prefabs.HEX_WATCHTOWER, Resources.Load<GameObject>("Prefabs/Hexes/Defense/Hex - Watchtower"));
+        prefabs.Add(Enums.Prefabs.HEX_MISSILE_COMPLEX, Resources.Load<GameObject>("Prefabs/Hexes/Defense/Hex - Missile Complex"));
+        prefabs.Add(Enums.Prefabs.HEX_LASER_TOWER, Resources.Load<GameObject>("Prefabs/Hexes/Defense/Hex - Laser Tower"));
+        prefabs.Add(Enums.Prefabs.HEX_AUTO_MISSILE_COMPLEX, Resources.Load<GameObject>("Prefabs/Hexes/Defense/Hex - Auto Missile Complex"));
+        prefabs.Add(Enums.Prefabs.HEX_AUTO_LASER_TOWER, Resources.Load<GameObject>("Prefabs/Hexes/Defense/Hex - Auto Laser Tower"));
+
+        prefabs.Add(Enums.Prefabs.HEX_RESEARCH_LAB, Resources.Load<GameObject>("Prefabs/Hexes/Research/Hex - Research Lab"));
+        prefabs.Add(Enums.Prefabs.HEX_RESEARCH_COLLEGE, Resources.Load<GameObject>("Prefabs/Hexes/Research/Hex - Research College"));
+        prefabs.Add(Enums.Prefabs.HEX_RESEARCH_INSTITUTE, Resources.Load<GameObject>("Prefabs/Hexes/Research/Hex - Research Institute"));
+        prefabs.Add(Enums.Prefabs.HEX_MULTIBRAIN_COMPLEX, Resources.Load<GameObject>("Prefabs/Hexes/Research/Hex - MultiBrain Complex"));
+        prefabs.Add(Enums.Prefabs.HEX_QUANTUM_BRAIN, Resources.Load<GameObject>("Prefabs/Hexes/Research/Hex - Quantum Brain"));
+
+        prefabs.Add(Enums.Prefabs.HEX_STOCKPILE, Resources.Load<GameObject>("Prefabs/Hexes/Isolium/Hex - Stockpile"));
+        prefabs.Add(Enums.Prefabs.HEX_STOREHOUSE, Resources.Load<GameObject>("Prefabs/Hexes/Isolium/Hex - Storehouse"));
+        prefabs.Add(Enums.Prefabs.HEX_WAREHOUSE, Resources.Load<GameObject>("Prefabs/Hexes/Isolium/Hex - Warehouse"));
+        prefabs.Add(Enums.Prefabs.HEX_DEPOT, Resources.Load<GameObject>("Prefabs/Hexes/Isolium/Hex - Depot"));
+        prefabs.Add(Enums.Prefabs.HEX_DISTRIBUTION_CENTER, Resources.Load<GameObject>("Prefabs/Hexes/Isolium/Hex - Distribution Center"));
     }
 
     public static GameObject GetPrefab(Enums.Prefabs prefab)
